91de8135679b91bdb49bfdd4a452b75deac3bc0d
DESKTOP-V66AUH1\Vashdev Heerani <vashdevheerani@gmail.com>
2017-09-11 10:33:17 -0700

    Initial update

---
+++ SportsStore/SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product
+        };
+    }
+}
